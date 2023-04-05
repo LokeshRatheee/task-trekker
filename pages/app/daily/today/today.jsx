5dee32d64067f9e385cf5090db99a6d7e1069a9f
LokeshRatheee <youstupidgotohell@gmail.com>
2023-04-05 17:58:10 +0530

    5-04

--- pages/app/daily/today/today.jsx
+++ pages/app/daily/today/today.jsx
@@ -9,7 +9,7 @@ export default function Today() {
           <span className={styles.today}>Today</span>
           <div className={styles.tasks}>4/6 completed</div>
           <div className={styles.tasklist}>
-            <ul>
+            <ul className={styles.xyz}>
               <li className={styles.list1}>
                 <Image
                   src="/daily/checklist.png"
@@ -56,6 +56,69 @@ export default function Today() {
                   </div>
                 </div>
               </li>
+              <li className={styles.list1}>
+                <Image
+                  src="/daily/checklist.png"
+                  alt="Picture of the author"
+                  width={26}
+                  height={25}
+                  className={styles.checkicon}
+                />
+                <div className={styles.frame}>
+                  <div className={styles.leftside}>
+                    <span className={styles.listheading}>
+                      Cooking a salmon sushi
+                    </span>
+                    <p className={styles.listp}>
+                      Salmon and tuna i think is good for dinner, i wanna make
+                      it today :D
+                    </p>
+                  </div>
+                  <div className={styles.rightsideicons}>
+                    <Image
+                      src="/daily/bi_pencil-square.png"
+                      alt="Picture of the author"
+                      width={26}
+                      height={25}
+                      className={styles.pencilsquare}
+                    />
+                    <Image
+                      src="/daily/bi_three-dots.png"
+                      alt="Picture of the author"
+                      width={26}
+                      height={25}
+                      className={styles.pencilsquare}
+                    />
+                    <button className={styles.deletebtn}>
+                      <Image
+                        src="/daily/bi_trash-fill.png"
+                        alt="Picture of the author"
+                        width={26}
+                        height={25}
+                        className={styles.deleteicon}
+                      />
+                      <span className={styles.btndeletetext}>Delete</span>
+                    </button>
+                  </div>
+                </div>
+              </li>
+              <li className={styles.addtaskli}>
+              <Image
+                  src="/daily/fluent_add-circle-32-filled.png"
+                  alt="Picture of the author"
+                  width={26}
+                  height={25}
+                  className={styles.addtaskicon}
+                />
+                <span className={styles.addtask}>Add task</span>
+              </li>
+              <li >
+                <div className={styles.inputboxs}>
+                <input type="text" placeholder="Task title" className={styles.tasktitle} />
+                <input type="text"  placeholder="Description" className={styles.description}/>
+                </div>
+              </li>
+
             </ul>
           </div>
         </div>
